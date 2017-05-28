61be7373c586b4e4f0085ce52c9fff0559b8975d
Janne <calm+git@iki.fi>
2017-05-28 17:38:43 +0300

    Added support for remote controlled IO

--- lua/ds1820_mqtt_mokki.lua
+++ lua/ds1820_mqtt_mokki.lua
@@ -1,17 +1,41 @@
+IO_PIN = 3 -- gpio0
+state = 0 -- OFF
 
 t = require('ds18b20')
-t.setup(4)
+t.setup(4) -- gpio2
 addrs = t.addrs()
 for i=1, table.getn(addrs) do print(t.read(addrs[i])) end
 
-mqtt = mqtt.Client("esp2", 120)
-mqtt:on("connect", function(con) print ("Connected") end)
-mqtt:on("offline", function(con)
+m = mqtt.Client("esp2", 120)
+m:on("offline", function(con)
   print ("Offline")
   node.restart()
 end)
-mqtt:connect("192.168.2.1", 1883, 0, 1, function(client) print("connected") end, 
-                                     function(client, reason) print("failed reason: "..reason) end)
+
+m:on("message", function(conn, topic, data)
+  print(topic .. ":" )
+  if data ~= nil then
+    if state == 0 then
+      print("Switching ON")
+      gpio.write(IO_PIN, gpio.HIGH)
+      state = 1
+    else
+      print("Swithcing OFF")
+      gpio.write(IO_PIN, gpio.LOW)
+      state = 0
+    end
+  end
+end)
+
+m:connect("192.168.0.1", 1883, 0, 1,
+  function(client)
+    print("Connected to MQTT")
+    m:subscribe("trigger", 0, function(conn) print("Trigger subscribed successfully") end)
+  end,
+  function(client, reason)
+    print("Failed to connect, reason: " .. reason)
+    node.restart()
+  end)
 
 tmr.alarm(0, 60000, 1, function()
     print('Measuring..')
@@ -20,7 +44,7 @@ tmr.alarm(0, 60000, 1, function()
         output = output .. t.read(addrs[i]) .. " "
     end
     print(output)
-    mqtt:publish("mokkimittaukset", output, 2, 0, function(conn)
-      print("sent")
+    m:publish("mokkimittaukset", output, 2, 0, function(conn)
+      print("Sent data")
     end)
 end)
--- lua/init_ds1820_mokki.lua
+++ lua/init_ds1820_mokki.lua
@@ -1,4 +1,8 @@
+IO_PIN = 3 -- gpio0
+
 function startup()
+    gpio.mode(IO_PIN, gpio.OUTPUT)
+    gpio.write(IO_PIN, gpio.LOW)
     if abort == true then
         print('Start aborted!')
         return
@@ -7,6 +11,6 @@ function startup()
     dofile('ds1820_mqtt_mokki.lua')
     end
 
-abort = false
 uart.setup(0, 9600, 8, 0, 1, 1)
+abort = false
 tmr.alarm(0,10000,0,startup)
